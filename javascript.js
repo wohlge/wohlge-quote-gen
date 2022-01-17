9e6c9693cd23486e931b0a4fe586b5cf9483ed90
wohlge <97902625+wohlge@users.noreply.github.com>
2022-01-17 16:06:25 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 var quotes = [
-	'The way to get started is to quit talking and begin doing. -Walt Disney',
-	'The pessimist sees difficulty in every opportunity. The optimisty sees the opportunity in every difficulty. - Winston Churchill',
-	'Don\'t let yesterday take up too much of today. -Will Rogers'
+	'<i>"\You have power over your mind - not outside events. Realize this, and you will find strength."\<i/> ― Marcus Aurelius, Meditations',
+	'"\The happiness of your life depends upon the quality of your thoughts."\ ― Marcus Aurelius, Meditations',
+	'"\If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment."\ ― Marcus Aurelius, Meditations',
+	'"\When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love ..."\ ― Marcus Aurelius, Meditations',
+	'"\It is not death that a man should fear, but he should fear never beginning to live."\ ― Marcus Aurelius, Meditations',
+	'"\Our life is what our thoughts make it."\ ― Marcus Aurelius, Meditations',
+	'"\The happiness of your life depends upon the quality of your thoughts."\ ― Marcus Aurelius, Meditations',
+	'"\Very little is needed to make a happy life; it is all within yourself in your way of thinking."\ - Marcus Aurelius, Meditations',
+	'"\Adopt a mindset of perseverance, convince yourself that you will 100% achieve your goals and never give up. Be critical of yourself but also have faith in yourself."\ - Concerned Ape'
 ]
 
 function newQuote() {
